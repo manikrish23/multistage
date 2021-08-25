bb8f8ff1bebee59363b8766c35d337bec87f1dfb
Manikandan Krishnan <manikandan.k@BRILLIO.COM>
2021-08-25 17:30:36 +0530

    upload latest files

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tfmainrg"
+        storage_account_name = "tfstorageacc0001"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+  }
+
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
+# Configure the Microsoft Azure Provider
+ 
+resource "azurerm_resource_group" "tf_test" {
+    name = "tfmainrg"
+    location = "Australia East"
+}
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "manikirish"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "manikrish/weatherapi:${var.imagebuild}"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
