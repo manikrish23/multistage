759182ea3bc2a2b8334a19dcdbca60828635caa2
Manikandan Krishnan <manikandan.k@BRILLIO.COM>
2021-08-25 23:42:21 +0530

    test

--- main.tf
+++ main.tf
@@ -2,12 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "~>2.39.0"
+      
     }
   }
-}
-provider "azurerm" {
+  provider "azurerm" {
   features {}
+}
+
 }
 terraform {
     backend "azurerm" {
